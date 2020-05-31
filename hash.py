61820e52834f2a366983e495cb0e134ddb590c83
Chen Nuode <chennuode2012@gmail.com>
2020-05-31 17:52:26 +0800

    Hello

--- hash.py
+++ hash.py
@@ -3,6 +3,7 @@ from hashlib import sha256
 salt = "verygoodproject"
 
 def hash(password):
+    
     return password
     #password += salt
     #return sha256(password.encode("utf-8")).hexdigest()
